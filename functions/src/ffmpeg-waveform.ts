32a41e9a4858c4a67ecf41e0e8c669a0e1782b25
Koen Schmeets <hello@koenschmeets.nl>
2020-02-27 15:53:30 +0100

    Add simpler ffmpeg parser, add ffprobe for file info

--- functions/src/ffmpeg-waveform.ts
+++ functions/src/ffmpeg-waveform.ts
@@ -1,11 +1,30 @@
-const Parser = require('binary-parser').Parser
-
+// scale function
+function scale(value: number, fromMin: number, fromMax: number, toMin: number, toMax: number) {
+  const fromRange = fromMax - fromMin
+  const toRange = toMax - toMin
+  return ((value - fromMin) * toRange / fromRange) + toMin
+}
+  
 function parse(data: Buffer) {
-  const parser = new Parser()
-    .endianess('little')
-    .array('waves', { type: 'int16le', readUntil: 'eof' })
-
-  const waves = parser.parse(data).waves
+  let oddByte = null
+  let channel = 0
+  const waves = []
+  let val
+  let i = 0
+  const dataLen = data.length
+  // If there is a leftover byte from the previous block, combine it with the
+  // first byte from this block
+  if (oddByte !== null) {
+    val = ((data.readInt8(i++) << 8) | oddByte) / 32767.0
+    waves.push(val)
+    channel = ++channel % 2
+  }
+  for (; i < dataLen; i += 2) {
+    val = data.readInt16LE(i) / 32767.0
+    waves.push(val)
+    channel = ++channel % 2
+  }
+  oddByte = (i < dataLen) ? data.readUInt8(i) : null
 
   // figure out the extremes
   let min = Infinity
@@ -22,13 +41,6 @@ function parse(data: Buffer) {
     max = Math.abs(min)
   }
 
-  // scale function
-  function scale(value: number, fromMin: number, fromMax: number, toMin: number, toMax: number) {
-    const fromRange = fromMax - fromMin
-    const toRange = toMax - toMin
-    return ((value - fromMin) * toRange / fromRange) + toMin
-  }
-
   // resample
   const resampledWaves = []
   for (let j = 0; j < waves.length; j++) {
@@ -37,5 +49,38 @@ function parse(data: Buffer) {
 
   return resampledWaves
 }
+
+// const Parser = require('binary-parser').Parser
+
+// function parse(data: Buffer) {
+//   const parser = new Parser()
+//     .endianess('little')
+//     .array('waves', { type: 'int16le', readUntil: 'eof' })
+
+//   const waves = parser.parse(data).waves
+//   console.log(waves)
+//   // figure out the extremes
+//   let min = Infinity
+//   let max = -Infinity
+//   for (const wave of waves) {
+//     if (min > wave) { min = wave }
+//     if (max < wave) { max = wave }
+//   }
+
+//   // use the largest value away from 0
+//   if (Math.abs(min) < max) {
+//     min = -max
+//   } else {
+//     max = Math.abs(min)
+//   }
+
+//   // resample
+//   const resampledWaves = []
+//   for (let j = 0; j < waves.length; j++) {
+//     resampledWaves[j] = Math.round(Math.abs(scale(waves[j], min, max, -1, 1)) * 10000) / 10000
+//   }
+
+//   return resampledWaves
+// }
 
 export default parse
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,26 +4,58 @@ import ffmpegWaveform from './ffmpeg-waveform'
 import { spawn } from 'child_process'
 import sha1 from 'sha1'
 import admin from 'firebase-admin'
+import fs from 'fs'
+const downsampler = require("downsample-lttb")
 
 admin.initializeApp(functions.config().firebase);
 
 const ffmpegPath = require('@ffmpeg-installer/ffmpeg').path
+const ffprobePath = require('@ffprobe-installer/ffprobe').path
 
-function urlToWaveform(url: string): Promise<number[]> {
+function downsample(numbers: number[], targetLength: number) {
+    const pairs = numbers.map((number, i) => [i, number])
+    return downsampler.processData(pairs, targetLength)
+        .map((pair: [number, number]) => pair[1])
+}
+
+function urlToWaveform(url: string): Promise<{ peaks: number[], data: any }> {
     return fetch(url)
         .then(res => {
-            return new Promise((resolve) => {
-                const ffmpeg = spawn(ffmpegPath, ['-i', 'pipe:', '-ac', '1', '-filter:a', 'aresample=8000', '-map', '0:a', '-c:a', 'pcm_s16le', '-f', 'data', '-']);
-                const bufs: Buffer[] = []
-                ffmpeg.stdout.on('data', (data) => bufs.push(data))
-                ffmpeg.on('close', (code) => resolve(ffmpegWaveform(Buffer.concat(bufs))))
-                res.body.pipe(ffmpeg.stdin)
+            return new Promise((resolve, reject) => {
+                const hash = sha1(url)
+                const tmpFile = '/tmp/' + hash
+                const writeStream = fs.createWriteStream(tmpFile)
+                res.body.pipe(writeStream)
+                writeStream.on('error', reject)
+                writeStream.on('close', () => {
+                    const ffprobe = spawn(ffprobePath, ['-i', tmpFile, '-v', 'quiet', '-select_streams', 'a:0', '-print_format', 'json', '-show_format', '-show_streams', '-hide_banner'])
+                    const probeBufs: Buffer[] = []
+                    ffprobe.stdout.on('data', (data) => probeBufs.push(data))
+                    ffprobe.on('close', () => {
+                        const ffprobeData = JSON.parse(Buffer.concat(probeBufs).toString())
+                        let sampleRate = 1000
+                        if (ffprobeData.format.duration < 1) {
+                            sampleRate = sampleRate / ffprobeData.format.duration
+                        }
+                        const ffmpeg = spawn(ffmpegPath, ['-i', tmpFile, '-ac', '1', '-filter:a', 'aresample=' + sampleRate, '-map', '0:a', '-c:a', 'pcm_s16le', '-f', 'data', '-']);
+                        const bufs: Buffer[] = []
+                        ffmpeg.stdout.on('data', (data) => bufs.push(data))
+                        ffmpeg.on('close', () => {
+                            fs.unlinkSync(tmpFile)
+                            const peaks = ffmpegWaveform(Buffer.concat(bufs))
+                            resolve({
+                                data: ffprobeData,
+                                peaks: downsample(peaks, 2000)
+                            })
+                        })
+                    })
+                })
             })
         })
 }
 
 export const waveformData = functions.https.onRequest((request, response) => {
-    console.log(request.body)
+    // console.log(request.body)
     if (Array.isArray(request.body)) {
         const promises: Promise<any>[] = request.body.map(url => {
             const hash = sha1(url)
@@ -32,21 +64,23 @@ export const waveformData = functions.https.onRequest((request, response) => {
                 .then(snap => {
                     const value = snap.val()
                     if (value) {
-                        console.log('from cache', typeof value)
-                        return value
+                        return {
+                            data: JSON.parse(value.data),
+                            peaks: JSON.parse(value.peaks)
+                        }
                     } else {
-                        console.log('downloading')
-                        return urlToWaveform(url).then(peaks => {
-                            console.log('got peaks', peaks.length)
-                            const peaksStr = JSON.stringify(peaks)
-                            return ref.set(peaksStr)
-                                .then(() => peaksStr)
+                        return urlToWaveform(url).then(info => {
+                            console.log('peaks', info.peaks.length)
+                            const dataStr = JSON.stringify(info.data)
+                            const peaksStr = JSON.stringify(info.peaks)
+                            return ref.set({ data: dataStr, peaks: peaksStr })
+                                .then(() => info)
                         })
                     }
                 })
         })
-        Promise.all(promises).then(allPeaks => {
-            response.send(`[${allPeaks.join(',')}]`)
+        Promise.all(promises).then(allInfo => {
+            response.send(JSON.stringify(allInfo))
         }).catch(err => {
             response.status(400)
             response.send(err.message)        

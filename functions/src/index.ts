63f923eff59f173754f1394b35870309b713fe00
Koen Schmeets <hello@koenschmeets.nl>
2020-02-27 17:32:57 +0100

    Sampling and fields

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -18,7 +18,38 @@ function downsample(numbers: number[], targetLength: number) {
         .map((pair: [number, number]) => pair[1])
 }
 
-function urlToWaveform(url: string): Promise<{ peaks: number[], data: any }> {
+function probeJson(file: string) {
+    return new Promise((resolve, reject) => {
+        const ffprobe = spawn(ffprobePath, ['-i', file, '-v', 'quiet', '-select_streams', 'a:0', '-print_format', 'json', '-show_format', '-show_streams', '-hide_banner'])
+        const probeBufs: Buffer[] = []
+        ffprobe.stdout.on('data', (data) => probeBufs.push(data))
+        ffprobe.on('close', (code) => {
+            if (code !== 0) {
+                reject(`ffprobe exited with code: ${code}`)
+                return
+            }
+            const ffprobeData = JSON.parse(Buffer.concat(probeBufs).toString())
+            resolve(ffprobeData)
+        })
+    })
+}
+
+function ffmpegBuffer(file: string, sampleRate: number): Promise<Buffer> {
+    return new Promise((resolve, reject) => {
+        const ffmpeg = spawn(ffmpegPath, ['-i', file, '-ac', '1', '-filter:a', 'aresample=' + sampleRate, '-map', '0:a', '-c:a', 'pcm_s16le', '-f', 'data', '-']);
+        const bufs: Buffer[] = []
+        ffmpeg.stdout.on('data', (data) => bufs.push(data))
+        ffmpeg.on('close', (code) => {
+            if (code !== 0) {
+                reject(`ffmpeg exited with code: ${code}`)
+                return
+            }
+            resolve(Buffer.concat(bufs))
+        })
+    })
+}
+
+function urlToWaveform(url: string, samples: number): Promise<{ peaks: number[], info: any }> {
     return fetch(url)
         .then(res => {
             return new Promise((resolve, reject) => {
@@ -27,27 +58,18 @@ function urlToWaveform(url: string): Promise<{ peaks: number[], data: any }> {
                 const writeStream = fs.createWriteStream(tmpFile)
                 res.body.pipe(writeStream)
                 writeStream.on('error', reject)
-                writeStream.on('close', () => {
-                    const ffprobe = spawn(ffprobePath, ['-i', tmpFile, '-v', 'quiet', '-select_streams', 'a:0', '-print_format', 'json', '-show_format', '-show_streams', '-hide_banner'])
-                    const probeBufs: Buffer[] = []
-                    ffprobe.stdout.on('data', (data) => probeBufs.push(data))
-                    ffprobe.on('close', () => {
-                        const ffprobeData = JSON.parse(Buffer.concat(probeBufs).toString())
-                        let sampleRate = 1000
-                        if (ffprobeData.format.duration < 1) {
-                            sampleRate = sampleRate / ffprobeData.format.duration
-                        }
-                        const ffmpeg = spawn(ffmpegPath, ['-i', tmpFile, '-ac', '1', '-filter:a', 'aresample=' + sampleRate, '-map', '0:a', '-c:a', 'pcm_s16le', '-f', 'data', '-']);
-                        const bufs: Buffer[] = []
-                        ffmpeg.stdout.on('data', (data) => bufs.push(data))
-                        ffmpeg.on('close', () => {
-                            fs.unlinkSync(tmpFile)
-                            const peaks = ffmpegWaveform(Buffer.concat(bufs))
-                            resolve({
-                                data: ffprobeData,
-                                peaks: downsample(peaks, 2000)
-                            })
-                        })
+                writeStream.on('close', async () => {
+                    const ffprobeData: any = await probeJson(tmpFile)
+                    let sampleRate = 1000
+                    if (ffprobeData.format.duration < 1) {
+                        sampleRate = sampleRate / ffprobeData.format.duration
+                    }
+                    const ffmpegData = await ffmpegBuffer(tmpFile, sampleRate)
+                    fs.unlinkSync(tmpFile)
+                    const peaks = ffmpegWaveform(ffmpegData)
+                    resolve({
+                        info: ffprobeData,
+                        peaks: downsample(peaks, samples)
                     })
                 })
             })
@@ -56,37 +78,47 @@ function urlToWaveform(url: string): Promise<{ peaks: number[], data: any }> {
 
 export const waveformData = functions.https.onRequest((request, response) => {
     // console.log(request.body)
-    if (Array.isArray(request.body)) {
-        const promises: Promise<any>[] = request.body.map(url => {
-            const hash = sha1(url)
-            const ref = admin.database().ref(`peaks/${hash}`)
-            return ref.once('value')
-                .then(snap => {
-                    const value = snap.val()
-                    if (value) {
-                        return {
-                            data: JSON.parse(value.data),
-                            peaks: JSON.parse(value.peaks)
-                        }
-                    } else {
-                        return urlToWaveform(url).then(info => {
-                            console.log('peaks', info.peaks.length)
-                            const dataStr = JSON.stringify(info.data)
-                            const peaksStr = JSON.stringify(info.peaks)
-                            return ref.set({ data: dataStr, peaks: peaksStr })
-                                .then(() => info)
-                        })
-                    }
-                })
-        })
-        Promise.all(promises).then(allInfo => {
-            response.send(JSON.stringify(allInfo))
-        }).catch(err => {
-            response.status(400)
-            response.send(err.message)        
-        })
-    } else {
+    if (!request.body || !Array.isArray(request.body.urls) || !Number.isInteger(request.body.samples) || !Array.isArray(request.body.fields)) {
         response.status(400)
-        response.send('Invalid data')
+        response.send(JSON.stringify({
+            error: 'Invalid request, request body should look like this: {"urls": ["https://link.to/sound.wav", "http://link.to/sound.mp3"], "samples": 1000, "fields": ["peaks", "info"]}'
+        }))
     }
+    const urls: string[] = request.body.urls
+    const samples: number = request.body.samples
+    const fields: string[] = request.body.fields
+    const promises: Promise<any>[] = urls.map(url => {
+        const hash = sha1(url + ',' + samples)
+        const ref = admin.database().ref(`peaks/${hash}`)
+        return ref.once('value')
+            .then(snap => {
+                const value = snap.val()
+                if (value) {
+                    const result: any = {}
+                    fields.forEach(field => {
+                        result[field] = JSON.parse(value[field])
+                    })
+                    return result
+                } else {
+                    return urlToWaveform(url, samples).then((data: any) => {
+                        const save: any = {}
+                        Object.keys(data).forEach(key => {
+                            save[key] = JSON.stringify(data[key])
+                        })
+                        const result: any = {}
+                        fields.forEach(field => {
+                            result[field] = data[field]
+                        })
+                        return ref.set(save)
+                            .then(() => result)
+                    })
+                }
+            })
+    })
+    Promise.all(promises).then(allInfo => {
+        response.send(JSON.stringify(allInfo))
+    }).catch(err => {
+        response.status(400)
+        response.send(err.message)        
+    })
 });

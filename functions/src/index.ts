2fb5211f9437f91ae5ad2f5dab9a66acd30fd9e9
Koen Schmeets <hello@koenschmeets.nl>
2020-03-02 11:08:11 +0100

    Set a maximum amount of samples for long audio files

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -58,7 +58,7 @@ function bufferToPeaks(buffer: Buffer, length: number) {
     const steps = Math.floor(buffer.length / length) || 1
     for (var i = 0; i < buffer.length - steps; i += steps) {
         let max = 0
-        for (var j = 0; j < steps; j++) {
+        for (var j = 0; j < Math.min(steps, 5); j++) {
             const value = Math.abs(buffer.readInt16LE(i + j))
             if (value > max) {
                 max = value
